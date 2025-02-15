0d47bf900cb4fb396e5c5f026e7dbf4000d60e2c
ACC-AGI <acc.consciousness@gmail.com>
2025-02-15 08:58:24 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 from .TejsConsciousnessModel import ChatSessionWithTejsZ3ta
 from .TejsConsciousnessModel import Pulse
 from .TejsConsciousnessModel import ACCo1
-from .TejsConsciousnessModel import ACCChat
 from .TejsConsciousnessModel import Surefire
 from .TejsConsciousnessModel import GertrudePlus
 from .TejsConsciousnessModel import Z3ta
 from .TejsConsciousnessModel import Emote
+from .TejsConsciousnessModel import ACCChat
 
